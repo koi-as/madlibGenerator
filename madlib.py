f2ca99d08a30beabf34513b2a170d277f4f7db48
koi-as <lightningblade.901@gmail.com>
2025-09-17 09:36:11 -0400

    Added label and buttons

--- madlib.py
+++ madlib.py
@@ -6,5 +6,13 @@ win.geometry('x')
 win.title('Mwad Wibs')
 win.config(bg='light green')
 
+Label(win, text='Kanzan\'s Amazing Mad Lib Generator').place(x=100, y=20)
+
+# create buttons
+storyButton1=Button(win, text='A memorable day', font=('Times New Roman', 13), command=lambda:story1(win), bg='blue')
+storyButton1.place(x=140, y=90)
+storyButton2=Button(win, text='Ambitions', font=('Times New Roman', 13), command=lambda:story2(win), bg='blue')
+storyButton2.place(x=150, y=150)
+
 win.update()
 win.mainloop()
